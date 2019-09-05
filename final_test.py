7742b14b0bdbda5bbb168d547b2b0e7b534714ff
Sanket Shirode <sanket@prosperasoft.com>
2019-09-05 16:43:48 +0530

    new lambda

---
+++ final_test.py
@@ -0,0 +1,11 @@
+import json
+
+def sum(n=10):
+    sum_of_no = 0
+    for x in range(n+1):
+        sum_of_no = x + sum_of_no
+    return sum_of_no
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(sum())
